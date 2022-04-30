3a4e663285bb26777dba6ff51f2a17e7d6efdf24
CampanelliEmanuele <emanuele.campanelli@studio.unibo.it>
2022-04-30 23:52:40 +0200

    Bump: now there's 1 func to set {high|low} a bit

--- scripts/cppFiles_test/bitwiseOperations.cpp
+++ scripts/cppFiles_test/bitwiseOperations.cpp
@@ -2,17 +2,24 @@
 
 using namespace std;
 
-int addNewBit(int newBit_pos, int SR_status) {
-    int newBit = 1;
-    newBit_pos--;                           /* decrements cuz newBis is already := ...0001 */
-    for (int i = 0; i < newBit_pos; i++)
-        newBit = newBit << 1;
-    return SR_status | newBit;
+int change_bit_status(int SR_status, int bitPos, int set_bit_high) {
+    if (set_bit_high == 0 || set_bit_high == 1) {
+        int iter = 1;
+        for (int i = 0; i < bitPos; i++)
+            iter = iter << 1;
+        if (set_bit_high == 1)
+            return SR_status | iter;
+        else
+            return SR_status ^ iter;
+    } else
+        return 0;
 }
 
 int main () {
 
-    printf("%c%d%c", '\n', addNewBit(5,15), '\n');  /* print: 31 -> ..00011111*/
+    printf("%d%c", change_bit_status(0,4,1), '\n');  /* from: ...00000000 to: ...00010000 */
+
+    printf("%d%c", change_bit_status(31,0,0), '\n');
 
     return 0;
 }
--- scripts/shiftRegister_handler/utils.h
+++ scripts/shiftRegister_handler/utils.h
@@ -1,11 +1,25 @@
 #ifndef utils
 #define utils
 
-int addNewBit(int SR_status, int newBit_pos) {
-    int newBit = 1;
-    for (int i = 0; i < newBit_pos; i++)
-        newBit = newBit << 1;
-    return SR_status | newBit;
+/**
+ * @brief 
+ * 
+ * @param SR_status 
+ * @param bitPos REMEMBER THAT THE change_bit_status BIT COUNTER (OF THE BYTE) START FROM 0 
+ * @param set_bit_high 
+ * @return int 
+ */
+int change_bit_status(int SR_status, int bitPos, int set_bit_high) {
+    if (set_bit_high == 0 || set_bit_high == 1) {
+        int iter = 1;
+        for (int i = 0; i < bitPos; i++)
+            iter = iter << 1;
+        if (set_bit_high == 1)
+            return SR_status | iter;
+        else
+            return SR_status ^ iter;
+    } else
+        return 0;
 }
 
 #endif utils
