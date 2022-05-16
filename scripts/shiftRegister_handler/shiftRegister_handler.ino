71656cf7d78456df76fd868a4e886ff02354155b
Campanelli Emanuele <emanuele.campanelli@studio.unibo.it>
2022-05-17 00:05:44 +0200

    Bump: example file updated

--- scripts/shiftRegister_handler/shiftRegister_handler.ino
+++ scripts/shiftRegister_handler/shiftRegister_handler.ino
@@ -15,16 +15,20 @@ void setup() {
 void loop() {
 
   int SR_status = 7;
+
+  writeRegister(SR_status);           /* ..0111 -> SF q0, q1 and q2 are high */
   
-  SR_status = change_bit_status(SR_status, 3, 1);
+  delay(500);  
   
-  writeRegister(SR_status);
+  SR_status = change_bit_status(SR_status, 3, 1);   /* SR_status = 15 */ 
   
+  writeRegister(SR_status);           /* ..01111 -> SF q0, q1, q2 and q3 are high */
+
 }
 
 void writeRegister(unsigned int value) {
-  digitalWrite(LATCH, LOW);                     // Si abilita la scrittura sugli shift register
+  digitalWrite(LATCH, LOW);                     /* Enable writing to SF */
   shiftOut(DATA, CLOCK, MSBFIRST, value >> 8);
   shiftOut(DATA, CLOCK, MSBFIRST, value);
-  digitalWrite(LATCH, HIGH);                    // Si disabilita la scrittura sugli shift register
+  digitalWrite(LATCH, HIGH);                    /* Disable writing to SF */
 }
